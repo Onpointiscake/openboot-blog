165ec63a48e7bafd9fe6a89424d852c36d66cec8
Ana Fernández Valdés <aana.fdez@gmail.com>
2021-11-08 10:33:38 +0100

    Adding HomePage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import {
-  ChakraProvider, extendTheme, 
+  ChakraProvider,
+  extendTheme,
   withDefaultColorScheme,
-  theme as baseTheme, Button, 
-} from "@chakra-ui/react"
-
-
+  theme as baseTheme,
+} from "@chakra-ui/react";
 
 const customTheme = extendTheme(
   {
@@ -23,19 +22,21 @@ const customTheme = extendTheme(
         },
       },
     },
+    fonts: {
+      heading: "Monospace",
+      body: "Monospace",
+    },
   },
-  withDefaultColorScheme({ colorScheme: "brand" }),
-)
+  withDefaultColorScheme({ colorScheme: "brand" })
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={customTheme}>
-
       <App />
     </ChakraProvider>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
