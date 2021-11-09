f0ebb3a4040a377c25980447ab825c9ca804a8ae
Javier Suarez <45120507+Onpointiscake@users.noreply.github.com>
2021-11-09 11:28:36 +0100

    recogida de id (params)
    añadir recogida del id desde la url en cada artículo que se visualiza

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Link }  from "react-router-dom";
+import { Search2Icon } from '@chakra-ui/icons'
 
 export default function Article(props) {
     return (
-        <div>
-            <Link to={`/articulo/${props.id}`}><p>LEER AHORA</p></Link>
+        <div>                            {/* Cambiar el color de la lupa al del tema ?¿ No se hacerlo */}
+            <Link to={`/articulo/${props.id}`}>READ <Search2Icon w={6} h={6} color="green.500" /> </Link>
         </div>
     )
 }
