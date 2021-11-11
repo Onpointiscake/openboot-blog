db190db4df3d89bdbe6b39d951c3e9222c3ae99e
Ana Fernández Valdés <aana.fdez@gmail.com>
2021-11-11 12:46:18 +0100

    homepage

--- src/components/HomeList.jsx
+++ src/components/HomeList.jsx
@@ -1,35 +1,71 @@
-
 import Article from "./Article";
 
 export default function HomeList() {
   const properties = [
-    { id: 1, titulo: 'Este es el primer articulo', fechaCreacion: "08/11/2021", autor: "Rocio", categoria: "ReactJS", rutaImagen:"https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg" },
-    { id: 2, titulo: 'Este siguiente es el segundo', fechaCreacion: "08/11/2021", autor: "Pepe", categoria: "Java", rutaImagen:"https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg"  },
-    { id: 3, titulo: 'Otro más por aquí', fechaCreacion: "08/11/2021", autor: "Ana", categoria: "CSS", rutaImagen:"https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg" },
-    { id: 4, titulo: 'Este el cuarto y ultimo post', fechaCreacion:"08/11/2021", autor: "David", categoria: "HTML & CSS", rutaImagen:"https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg"  },
-    { id: 4, titulo: 'Este el cuarto y ultimo post', fechaCreacion:"08/11/2021", autor: "David", categoria: "Spring", rutaImagen:"https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg"  }
-
-  ]
-// no consigo que se vean las imagenes
+    {
+      id: 1,
+      titulo: "Este es el primer articulo",
+      fechaCreacion: "08/11/2021",
+      autor: "Rocio",
+      categoria: "ReactJS",
+      rutaImagen:
+        "https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg",
+    },
+    {
+      id: 2,
+      titulo: "Este siguiente es el segundo",
+      fechaCreacion: "08/11/2021",
+      autor: "Pepe",
+      categoria: "Java",
+      rutaImagen:
+        "https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg",
+    },
+    {
+      id: 3,
+      titulo: "Otro más por aquí",
+      fechaCreacion: "08/11/2021",
+      autor: "Ana",
+      categoria: "CSS",
+      rutaImagen:
+        "https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg",
+    },
+    {
+      id: 4,
+      titulo: "Este el cuarto y ultimo post",
+      fechaCreacion: "08/11/2021",
+      autor: "David",
+      categoria: "HTML & CSS",
+      rutaImagen:
+        "https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg",
+    },
+    {
+      id: 4,
+      titulo: "Este el cuarto y ultimo post",
+      fechaCreacion: "08/11/2021",
+      autor: "David",
+      categoria: "Spring",
+      rutaImagen:
+        "https://depor.com/resizer/CmzgBqm-lucY4hl9-eBi8dX72cM=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/IW7ZDXD2ONFHRFEK5FSZIURNIA.jpg",
+    },
+  ];
+  // no consigo que se vean las imagenes
   return (
-    <div className="container_article">
-    {properties.map((article)=> (
-      <div maxW="lg" borderWidth="2px" borderRadius="lg" >
-        <img scr={article.rutaImagen} alt=""/>
-         <h1 className="article_title">
-            {article.titulo}
-          </h1>
-          <Article key={article.id} id={article.id} titulo={article.titulo} imagen={article.rutaImagen}/>
+    <div className="homelist_container">
+      {properties.map((article) => (
+        <div className="card_article_container">
+          <img scr={article.rutaImagen} alt="" />
+          <h1 className="article_title">{article.titulo}</h1>
+          <Article
+            key={article.id}
+            id={article.id}
+            titulo={article.titulo}
+            imagen={article.rutaImagen}
+          />
           <h3 className="article_fechaCreacion">{article.fechaCreacion}</h3>
-            
-          <h3 className="article_autor">
-            {article.autor}
-          </h3>
-              
 
-          </div>
-
-    ))}
-</div>
+          <h3 className="article_autor">{article.autor}</h3>
+        </div>
+      ))}
+    </div>
   );
 }
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,31 +1,27 @@
 import React from "react";
 import HomeList from "../components/HomeList";
 import { Link } from "react-router-dom";
+
+import "../styles/startpage.css";
+
 //El link de ver mas lleva a la pagina de editar, no a la vista del articulo
 
 export default function StartPage() {
   return (
     <React.Fragment>
-      <div>
-        <div>
-          <h1 fontSize="60px" align="left">
-            Hey! 👋
-          </h1>
-          <h2 fontSize="40px" align="left">
-            ...start writing something
-          </h2>{" "}
+      <div className="container_home">
+        <div className="container_heading_home">
+          <h1 className="title_home">Hey! 👋</h1>
+          <h2 className="subtitle_home">...start writing something</h2>
           <Link to="/editorapp">
-            <button to="./EditorApp.js" variant="outline" size="lg">
+            <button className="newpost_home" to="./EditorApp.js">
               New Post
             </button>
-          </Link>
-          <Link to="/login">
-            <button to="./Login.js" variant="outline" size="lg">
-              Login
-            </button>
-          </Link>
-        </div>{" "}
-        <div maxW="div.xl">
+          </Link>{" "}
+        </div>
+
+        <Link to="/login"></Link>
+        <div>
           <HomeList />
         </div>
       </div>
